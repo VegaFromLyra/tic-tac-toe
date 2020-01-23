f8d6acfb1dad28ed50522893de6b942d7d77380a
ashab <asha.ab@gmail.com>
2020-01-22 19:31:38 -0500

    First commit - setting up initial js andcss

---
+++ index.js
@@ -0,0 +1,25 @@
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  render() {
+  }
+}
+
+class Game extends React.Component {
+  render() {
+  }
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
